20bfc3ffc55a9a4f52bd42bbdaf6c2cb256073f8
Browser8 <56970567+Browser8@users.noreply.github.com>
2022-06-24 04:12:46 +0100

    Saving bio now supports addons

--- Character Bio/media/lua/client/ISWriteBio.lua
+++ Character Bio/media/lua/client/ISWriteBio.lua
@@ -7,13 +7,13 @@ local FONT_SCALE = FONT_HGT_SMALL / 14
 
 
 function ISWriteBio:setVisible(visible)
-    self.javaObject:setVisible(visible)
+  self.javaObject:setVisible(visible)
 end
 
 function ISWriteBio:render()
-    local z = 15 * FONT_SCALE
-
-    self:drawTextCentre(self.targetPlayerName .. "'s Bio", self.width/2, z, 1,1,1,1, UIFont.Medium)
+  local z = 15 * FONT_SCALE
+  
+  self:drawTextCentre(self.targetPlayerName .. "'s Bio", self.width/2, z, 1,1,1,1, UIFont.Medium)
 end
 
 local function OnServerCommand(module, command, args)
@@ -24,37 +24,37 @@ local function OnServerCommand(module, command, args)
 end
 
 function ISWriteBio:createChildren()
-    local btnWid = 150 * FONT_SCALE
-    local btnHgt = FONT_HGT_SMALL + 5 * 2 * FONT_SCALE
-    local padBottom = 10 * FONT_SCALE
-
-    local height = 35 * FONT_HGT_SMALL + 4
-    self.entry = ISTextEntryBox:new("Loading...", padBottom, 30 * FONT_SCALE + FONT_HGT_MEDIUM, self.width - 20 * FONT_SCALE, height)
-    self.entry:initialise()
-    self.entry:instantiate()
-    self.entry:setMultipleLine(true)
-    self.entry.javaObject:setMaxLines(35)
-    self:addChild(self.entry)
-    Events.OnServerCommand.Add(OnServerCommand)
-    sendClientCommand("CharacterBio", "load", {self.targetPlayerUsername})
-
-    if self.canEdit then
-      self.save = ISButton:new(padBottom, self.height - padBottom - btnHgt, btnWid, btnHgt, "SAVE", self, ISWriteBio.onSave)
-      self.save:initialise()
-      self.save.borderColor = self.buttonBorderColor
-      self:addChild(self.save)
-    else
-      self.entry:setEditable(false)
-    end
-
-    self.cancel = ISButton:new(self.width - btnWid - padBottom, self.height - padBottom - btnHgt, btnWid, btnHgt, getText("UI_btn_close"), self, ISWriteBio.close)
-    self.cancel:initialise()
-    self.cancel.borderColor = self.buttonBorderColor
-    self:addChild(self.cancel)
+  local btnWid = 150 * FONT_SCALE
+  local btnHgt = FONT_HGT_SMALL + 5 * 2 * FONT_SCALE
+  local padBottom = 10 * FONT_SCALE
+  
+  local height = 35 * FONT_HGT_SMALL + 4
+  self.entry = ISTextEntryBox:new("Loading...", padBottom, 30 * FONT_SCALE + FONT_HGT_MEDIUM, self.width - 20 * FONT_SCALE, height)
+  self.entry:initialise()
+  self.entry:instantiate()
+  self.entry:setMultipleLine(true)
+  self.entry.javaObject:setMaxLines(35)
+  self:addChild(self.entry)
+  Events.OnServerCommand.Add(OnServerCommand)
+  sendClientCommand("CharacterBio", "load", {self.targetPlayerUsername})
+  
+  if self.canEdit then
+    self.save = ISButton:new(padBottom, self.height - padBottom - btnHgt, btnWid, btnHgt, "SAVE", self, ISWriteBio.onSave)
+    self.save:initialise()
+    self.save.borderColor = self.buttonBorderColor
+    self:addChild(self.save)
+  else
+    self.entry:setEditable(false)
+  end
+  
+  self.cancel = ISButton:new(self.width - btnWid - padBottom, self.height - padBottom - btnHgt, btnWid, btnHgt, getText("UI_btn_close"), self, ISWriteBio.close)
+  self.cancel:initialise()
+  self.cancel.borderColor = self.buttonBorderColor
+  self:addChild(self.cancel)
 end
 
 function ISWriteBio:onSave(button, x, y)
-  sendClientCommand("CharacterBio", "save", {self.targetPlayerUsername, self.entry:getText()})
+  sendClientCommand("CharacterBio", "save", {description = self.entry:getText()})
   self:close()
 end
 
@@ -65,19 +65,19 @@ function ISWriteBio:close()
 end
 
 function ISWriteBio:new(x, y, width, height, targetPlayer, canEdit)
-    local o = ISPanel:new(x, y, width, height)
-    setmetatable(o, self)
-    self.__index = self
-    o.variableColor = {r=0.9, g=0.55, b=0.1, a=1}
-    o.borderColor = {r=0.4, g=0.4, b=0.4, a=1}
-    o.backgroundColor = {r=0, g=0, b=0, a=0.8}
-    o.buttonBorderColor = {r=0.7, g=0.7, b=0.7, a=0.5}
-    o.moveWithMouse = true
-    o.targetPlayerName = targetPlayer:getDescriptor():getForename()
-	  o.targetPlayerUsername = targetPlayer:getUsername()
-    o.canEdit = canEdit
-    ISWriteBio.instance = o
-    return o
+  local o = ISPanel:new(x, y, width, height)
+  setmetatable(o, self)
+  self.__index = self
+  o.variableColor = {r=0.9, g=0.55, b=0.1, a=1}
+  o.borderColor = {r=0.4, g=0.4, b=0.4, a=1}
+  o.backgroundColor = {r=0, g=0, b=0, a=0.8}
+  o.buttonBorderColor = {r=0.7, g=0.7, b=0.7, a=0.5}
+  o.moveWithMouse = true
+  o.targetPlayerName = targetPlayer:getDescriptor():getForename()
+  o.targetPlayerUsername = targetPlayer:getUsername()
+  o.canEdit = canEdit
+  ISWriteBio.instance = o
+  return o
 end
 
 return ISWriteBio
--- Character Bio/media/lua/server/CharacterBioServer.lua
+++ Character Bio/media/lua/server/CharacterBioServer.lua
@@ -8,42 +8,45 @@ local CharacterBioStorage
 
 ----------------------------------- loading -----------------------------------
 local function Load()
-    local fileReaderObj = getFileReader("CharacterBioStorage.json", true)
-    local json = ""
-    local line = fileReaderObj:readLine()
-    while line ~= nil do
-        json = json .. line
-        line = fileReaderObj:readLine()
-    end
-    fileReaderObj:close()
-
-    if json and json ~= "" then
-        CharacterBioStorage = Json.Decode(json)
-    end
+  local fileReaderObj = getFileReader("CharacterBioStorage.json", true)
+  local json = ""
+  local line = fileReaderObj:readLine()
+  while line ~= nil do
+    json = json .. line
+    line = fileReaderObj:readLine()
+  end
+  fileReaderObj:close()
+  
+  if json and json ~= "" then
+    CharacterBioStorage = Json.Decode(json)
+    local fileWriterObj = getFileWriter("CharacterBioStorage.json", true, false)
+    fileWriterObj:write("")
+    fileWriterObj:close()
+  end
 end
 
 Events.OnInitGlobalModData.Add(function()
-	Load()
-
+  Load()
+  
   local newStorage = ModData.getOrCreate("CharacterBioStorage")
-
+  
   if CharacterBioStorage then
     for k, v in pairs(CharacterBioStorage) do
       newStorage[k] = {description = v}
     end
   end
-
+  
   CharacterBioStorage = newStorage
 end)
 -------------------------------------------------------------------------------
 
 Events.OnClientCommand.Add(function(module, command, player, args)
-	if module == "CharacterBio" then
+  if module == "CharacterBio" then
     if command == "save" then
-      CharacterBioStorage[args[1]].description = args[2]
+      CharacterBioStorage[player:getUsername()] = args
     elseif command == "load" then
       sendServerCommand(player, module, command, CharacterBioStorage[args[1]])
     end
-
+    
   end
 end)
